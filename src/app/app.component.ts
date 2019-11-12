77afc8a421a75e9b98b7a0e69fef37853785fc1f
Craig McKeachie <craigdmckeachie@gmail.com>
2019-11-12 16:26:13 -0500

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, timer } from 'rxjs';
+import { Observable, timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,15 @@ import { Observable, timer } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   timer$: Observable<number> = timer(0, 1000);
+  subscription: Subscription;
 
   ngOnInit(): void {
-    this.timer$.subscribe(secondsElapsed => console.log(secondsElapsed));
+    this.subscription = this.timer$.subscribe(secondsElapsed =>
+      console.log(secondsElapsed)
+    );
   }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
