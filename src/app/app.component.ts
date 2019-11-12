f117b6a769b4fb31feebc4902f5ec5b94aa30380
Craig McKeachie <craigdmckeachie@gmail.com>
2019-11-12 16:38:45 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, timer, Subscription } from 'rxjs';
+import { Observable, timer, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <p>{{ timer$ | async }}</p>
-  `,
+  template: ``,
   styles: []
 })
 export class AppComponent implements OnInit, OnDestroy {
   timer$: Observable<number> = timer(0, 1000);
+  private unsubscribe$ = new Subject<void>();
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.timer$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(secondsElapsed => console.log(secondsElapsed));
+  }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
